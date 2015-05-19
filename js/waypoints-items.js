2aab91335ede0784450f321180b73bee01f38db1
Daniela <just@addsalt.ca>
2015-05-19 13:53:58 -0400

    feat(css): add all title slide in animations

--- js/waypoints-items.js
+++ js/waypoints-items.js
@@ -1,20 +1,107 @@
 var $win = $(window);
-var $expPiece = $('.work-explorandes');
+
+// Work specific triggers
 var $expTrigger = $('#explorandes');
+var $expTitle = $('#explorandes .work-title');
+var $expSeeOn = $('#explorandes .work-seeon-l');
+//var $expPiece = $('.work-explorandes');
+
+var $airTrigger = $('#airugo');
+var $airTitle = $('#airugo .work-title');
+
+var $stinksTitle = $('#lovestinks .work-title');
+var $stinksSeeOn = $('#lovestinks .work-seeon-l');
+
+var $woodworksTrigger = $('#woodworks');
+var $woodworksTitle = $('#woodworks .work-title');
+
+var $usilTrigger = $('#usil');
+var $usilTitle = $('#usil .work-title');
+var $usilSeeOn = $('#usil .work-seeon-l');
+
+var $digestiveTrigger = $('#digestive');
+var $digestiveTitle = $('#digestive .work-title');
+var $digestiveSeeOn = $('#digestive .work-seeon-l');
+
+var $ipaTitle = $('#ipa .work-title');
+var $ipaSeeOn = $('#ipa .work-seeon-l');
+
+var $frontTrigger = $('#frontcrush');
+var $frontTitle = $('#frontcrush .work-title');
+var $frontSeeOn = $('#frontcrush2 .work-seeon-l');
+
+
+//Explorandes
+$expTrigger.waypoint(function() {
+	$expTitle.addClass('js-slideLeftFade');
+}, { offset:'65%' });
 
 $expTrigger.waypoint(function() {
-	$expPiece.addClass('js-exp-fade');
-}, { offset:'50%' });
+	$expSeeOn.addClass('js-slideRightFade');
+}, { offset:'65%' });
+
+//$expTrigger.waypoint(function() {
+//	$expPiece.addClass('js-exp-fade');
+//}, { offset:'65%' });
 
 
-var $title = $('.work-title');
+//AirUGo
+$airTrigger.waypoint(function() {
+	$airTitle.addClass('js-slideLeftFade');
+}, { offset:'65%' });
 
-$expTrigger.waypoint(function() {
-	$title.addClass('js-slideLeftFade');
-}, { offset:'75%' });
 
-var $seeOn = $('.work-seeon-l');
+//Love Stinks
+$airTrigger.waypoint(function() {
+	$stinksTitle.addClass('js-slideLeftFade');
+}, { offset:'65%' });
 
-$expTrigger.waypoint(function() {
-	$seeOn.addClass('js-slideRightFade');
-}, { offset:'75%' });
+$airTrigger.waypoint(function() {
+	$stinksSeeOn.addClass('js-slideRightFade');
+}, { offset:'65%' });
+
+
+// Woodworks 
+$woodworksTrigger.waypoint(function() {
+	$woodworksTitle.addClass('js-slideLeftFade');
+}, { offset:'65%' });
+
+
+// USIL
+$usilTrigger.waypoint(function() {
+	$usilTitle.addClass('js-slideLeftFade');
+}, { offset:'65%' });
+
+$usilTrigger.waypoint(function() {
+	$usilSeeOn.addClass('js-slideRightFade');
+}, { offset:'65%' });
+
+
+// Digestive
+$digestiveTrigger.waypoint(function() {
+	$digestiveTitle.addClass('js-slideLeftFade');
+}, { offset:'65%' });
+
+$digestiveTrigger.waypoint(function() {
+	$digestiveSeeOn.addClass('js-slideRightFade');
+}, { offset:'65%' });
+
+
+// IPA
+$digestiveTrigger.waypoint(function() {
+	$ipaTitle.addClass('js-slideLeftFade');
+}, { offset:'65%' });
+
+$digestiveTrigger.waypoint(function() {
+	$ipaSeeOn.addClass('js-slideRightFade');
+}, { offset:'65%' });
+
+
+// Frontcrush
+$frontTrigger.waypoint(function() {
+	$frontTitle.addClass('js-slideLeftFade');
+}, { offset:'65%' });
+
+$frontTrigger.waypoint(function() {
+	$frontSeeOn.addClass('js-slideRightFade');
+}, { offset:'65%' });
