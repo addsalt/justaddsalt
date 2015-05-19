369c8f14870b5bd1ec942323a74bfc04faf44063
Daniela <just@addsalt.ca>
2015-05-19 12:52:26 -0400

    feat(css): add work animation 1

--- js/waypoints-items.js
+++ js/waypoints-items.js
@@ -1,7 +1,20 @@
 var $win = $(window);
-var $navTop = $('.navtop-hid');
-var $areaTrigger = $('.area-trigger');
+var $expPiece = $('.work-explorandes');
+var $expTrigger = $('#explorandes');
 
-$areaTrigger.waypoint(function() {
-	$navTop.addClass('js-navtop-fade');
-}, { offset:'-5%' });
+$expTrigger.waypoint(function() {
+	$expPiece.addClass('js-exp-fade');
+}, { offset:'50%' });
+
+
+var $title = $('.work-title');
+
+$expTrigger.waypoint(function() {
+	$title.addClass('js-slideLeftFade');
+}, { offset:'75%' });
+
+var $seeOn = $('.work-seeon-l');
+
+$expTrigger.waypoint(function() {
+	$seeOn.addClass('js-slideRightFade');
+}, { offset:'75%' });
